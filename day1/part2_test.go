28617f8fd8ee0727c20aee126213d48a58e6d175
Anders Lundholm <anders.andlu.se>
2020-12-06 17:30:06 +0100

    Added tests.

--- day1/part2_test.go
+++ day1/part2_test.go
@@ -5,21 +5,21 @@ import (
 	"testing"
 )
 
-func TestMultiplyTwo(t *testing.T) {
+func TestMultiplyThree(t *testing.T) {
 	var tests = []struct {
 		data []int
 		want int
 	}{
-		{[]int{2019, 1}, 2019},
-		{[]int{1000, 10, 10, 1020}, 1020000},
-		{[]int{-20, 2040, 15}, -40800},
-		{[]int{2000, 2, 3, 20, 5, 5}, 40000},
+		{[]int{2018, 1, 1}, 2018},
+		{[]int{1000, 10, 10, 40, 980, 1020}, 39200000},
+		{[]int{-20, 2040, -30, 10, 15, 1}, -612000},
+		{[]int{2000, 2, 3, 20, 5, 15}, 150000},
 	}
 
 	for _, test := range tests {
 		testname := fmt.Sprintf("%d", test.data)
 		t.Run(testname, func(t *testing.T) {
-			ans, _ := multiplyTwo(test.data)
+			ans, _ := multiplyThree(test.data)
 			if ans != test.want {
 				t.Errorf("got %d, want %d", ans, test.want)
 			}
---
+++ day2/day2_test.go
@@ -0,0 +1,27 @@
+package day2
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestParseFile(t *testing.T) {
+	var tests = []struct {
+		passwordLine string
+		want         dbEntry
+	}{
+		{"1-3 a: abcde", dbEntry{1, 3, "a", "abcde"}},
+		{"1-3 b: cdefg", dbEntry{1, 3, "b", "cdefg"}},
+		{"2-9 c: ccccccccc", dbEntry{2, 9, "c", "ccccccccc"}},
+	}
+
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.passwordLine)
+		t.Run(testname, func(t *testing.T) {
+			ans, _ := parseFile(test.passwordLine)
+			if ans != test.want {
+				t.Errorf("got %v, want %v", ans, test.want)
+			}
+		})
+	}
+}
