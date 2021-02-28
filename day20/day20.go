c89216924012d8768fb67b2149e1770cff1c4f85
Anders Lundholm <anders.andlu.se>
2021-02-28 15:57:33 +0100

    Day 20 completed!

--- day20/day20.go
+++ day20/day20.go
@@ -1,6 +1,10 @@
 package day20
 
 import (
+	"fmt"
+	"image"
+	"math"
+	"regexp"
 	"strings"
 
 	"github.com/anderslundholm/advent_of_code_2020/pkg/util"
@@ -83,4 +87,98 @@ func assembleImage(input []string) int {
 		}
 	}
 	return multiplyCorners(matchedTiles)
+}
+
+// Tile ...
+type Tile []string
+
+func (t Tile) col(i int) (col string) {
+	for _, s := range t {
+		col += string(s[i])
+	}
+	return
+}
+
+func (t Tile) edges() []string {
+	return []string{t[0], t.col(len(t[0]) - 1), t[len(t)-1], t.col(0)}
+}
+
+func (t Tile) rotations() []Tile {
+	rot := make([]Tile, 8)
+	for r := 0; r < 8; r += 2 {
+		for _, s := range t {
+			rot[r] = append(rot[r], util.ReverseString(s))
+		}
+		for i := range t {
+			rot[r+1] = append(rot[r+1], util.ReverseString(t.col(i)))
+		}
+		t = rot[r+1]
+	}
+	return rot
+}
+
+func part2(input string) int {
+
+	split := strings.Split(strings.TrimSpace(string(input)), "\n\n")
+
+	tiles := map[int]Tile{}
+	counts := map[string]int{}
+	for _, s := range split {
+		var id int
+		fmt.Sscanf(s, "Tile %d:", &id)
+		tiles[id] = strings.Split(s, "\n")[1:]
+		for _, e := range tiles[id].edges() {
+			counts[e]++
+			counts[util.ReverseString(e)]++
+		}
+	}
+
+	imageSize, tileSize := int(math.Sqrt(float64(len(tiles)))), 10
+	img := make(Tile, imageSize*(tileSize-2))
+	order := map[image.Point]Tile{}
+
+	part1 := 1
+	for y := 0; y < imageSize; y++ {
+		for x := 0; x < imageSize; x++ {
+		findTile:
+			for i, t := range tiles {
+				for _, r := range t.rotations() {
+					if (y == 0 && counts[r[0]] == 1 ||
+						y != 0 && r[0] == order[image.Point{x, y - 1}][tileSize-1]) &&
+						(x == 0 && counts[r.col(0)] == 1 ||
+							x != 0 && r.col(0) == order[image.Point{x - 1, y}].col(tileSize-1)) {
+						if (y == 0 || y == imageSize-1) && (x == 0 || x == imageSize-1) {
+							part1 *= i
+						}
+
+						for i := 0; i < tileSize-2; i++ {
+							img[(tileSize-2)*y+i] += r[i+1][1 : tileSize-1]
+						}
+
+						order[image.Point{x, y}] = r
+						delete(tiles, i)
+						break findTile
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(part1)
+
+	mon := []string{"..................#.", "#....##....##....###", ".#..#..#..#..#..#..."}
+	nmon := 0
+	for _, r := range img.rotations() {
+		for y := 0; y < len(r)-len(mon); y++ {
+		findMon:
+			for x := 0; x < len(r[0])-len(mon[0]); x++ {
+				for i, s := range mon {
+					if match, _ := regexp.MatchString(s, r[y+i][x:x+len(s)]); !match {
+						continue findMon
+					}
+				}
+				nmon++
+			}
+		}
+	}
+	return strings.Count(strings.Join(img, ""), "#") - nmon*strings.Count(strings.Join(mon, ""), "#")
 }
--- day20/part2.go
+++ day20/part2.go
@@ -2,9 +2,8 @@ package day20
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
 
-	"github.com/anderslundholm/advent_of_code_2020/pkg/reader"
 	"github.com/anderslundholm/advent_of_code_2020/pkg/timer"
 )
 
@@ -12,12 +11,14 @@ import (
 func Part2() {
 	defer timer.ExecutionTimer("Part2")()
 
-	input, err := reader.ReadLines("day20/input.txt")
-	if err != nil {
-		log.Fatalf("Could not read lines: %v\n", err)
-	}
+	// input, err := reader.ReadLines("day20/input.txt")
+	// if err != nil {
+	// 	log.Fatalf("Could not read lines: %v\n", err)
+	// }
 
-	result := assembleImage(input)
+	input, _ := ioutil.ReadFile("day20/input.txt")
+
+	result := part2(string(input))
 
 	fmt.Printf("Day20 Part2 result: %v\n", result)
 }
